b56b36184c9f1211bd2219320708f10bcdb95458
jamc96 <jamc.mejia@gmail.com>
2018-12-14 16:57:51 -0600

    constructor migrated to type

--- src/app/Resource.ts
+++ src/app/Resource.ts
@@ -1,5 +1,5 @@
 export class Resource {
-    constructor(public name : string, public expropriative: boolean, public maxUsages: number) {
-    
-    }
+    name: string;
+    expropriative: boolean;
+    maxUsages: number;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -98,7 +98,7 @@ export class AppComponent {
     const expropriative = Math.floor(Math.random() * 2) + 1 == 1
     const maxUsages = expropriative ? 1 : Math.floor(Math.random() * 3) + 1;
 
-    const newResource = new Resource(name, expropriative, maxUsages)
+    const newResource = { name: name, expropriative:expropriative, maxUsages: maxUsages };
     this.resources.push(newResource);
 
     return newResource;
